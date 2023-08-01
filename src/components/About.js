1d8d4ad4ad1a94500aa565507e7a8c984b2e39ec
River Ferguson <riverferguson@gmail.com>
2023-07-31 19:02:20 -0700

    add bio component as child compontent to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Bio from './Bio'
 
 const About = () => {
   return (
-    <div></div>
+    <div className='about'>
+        <Bio />
+        <div className='about-div'>
+            <h2 id='about'> About Me </h2>
+            <h3 className='about-me-bio'></h3>
+        </div>
+    </div>
   )
 }
 

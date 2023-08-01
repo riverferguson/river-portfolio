3a86f9b324f3bf80b4dea5e48e8e57b93bd6f9a2
River Ferguson <riverferguson@gmail.com>
2023-07-31 18:34:37 -0700

    add component skeletons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom'
 import './App.css';
-import { Header } from './components/Header';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
 
 function App() {
   return (

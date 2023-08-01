7ab78742329f1b127127317031f4646d19949294
River Ferguson <riverferguson@gmail.com>
2023-08-01 14:05:43 -0700

    begin building my interests and profile image in bio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import ReactDOM from 'react-dom'
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Bio = () => {
   return (
-    <div></div>
+    <div className='bio'>
+      <div className='bio-image'>
+        <img src={null} alt="image not found"/>
+      </div>
+      <div className='interests'>
+        <h4 className='interest-header'>My Interests</h4>
+      </div>
+    </div>
   )
 }
 

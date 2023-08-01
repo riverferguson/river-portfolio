8a255f8d4683f33d0e7fd3901b5a3c46d831079d
River Ferguson <riverferguson@gmail.com>
2023-07-31 18:43:55 -0700

    add Social icons and Name header to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="fixedHeader">
+    <header className="App-header">
+        <div className="header-name">
+        <a className='header-link' href="/">River Ferguson</a>
+        </div>
+        
+        <div className="header-icons">
+        <ul className="header-icons">
+            <SocialIcon className='icon' url="https://www.linkedin.com/in/river-ferguson/" target="_blank"/>
+            <SocialIcon className='icon' url="https://github.com/riverferguson" target="_blank"/>
+        </ul>
+        </div>
+    </header>
+</div>
   )
 }
 

c4670cbb317687a1cb3f3efdf1e96b9b0edeee46
River Ferguson <riverferguson@gmail.com>
2023-07-31 18:44:35 -0700

    add about, projects, experience, contact, and resume links to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,24 @@ const Header = () => {
         <div className="header-name">
         <a className='header-link' href="/">River Ferguson</a>
         </div>
-        
+        <div className="header-list">
+        <ul>
+            <a className='list-link' href='#about'>About</a>
+            
+        </ul>
+        <ul>
+            <a className='list-link' href='#projects'>Projects</a>
+        </ul>
+        <ul>
+            <a className='list-link' href='#experience'>Experience</a>
+        </ul>
+        <ul>
+            <a className='list-link'  href='#contact' >Contact</a>
+        </ul>
+        <ul>
+            <a className='list-link'  href={null} target="_blank">Resume</a>
+        </ul>
+        </div>
         <div className="header-icons">
         <ul className="header-icons">
             <SocialIcon className='icon' url="https://www.linkedin.com/in/river-ferguson/" target="_blank"/>

5eda0bca0a564715c0977c85b57c71439468fc99
River Ferguson <riverferguson@gmail.com>
2023-08-01 14:59:48 -0700

    add description and software to boat project card

--- src/components/Boat.js
+++ src/components/Boat.js
@@ -4,13 +4,13 @@ const Boat = () => {
   return (
     <div className='project-card'>
     <img src={null}></img>
-    <h2 className='project-name'>Salty's Surf Co.</h2>
-    <h4 className='project-description'>A user-friendly online surf shop intended for logged in users to buy and review surf products</h4>
+    <h2 className='project-name'>Pacific Boat Club</h2>
+    <h4 className='project-description'>An app designed for boat enthusiests to browse and list their boats while logged in</h4>
     <h5 className='software'>Software</h5>
     <h6 className='software-used'>Flask Python React</h6>
     <br></br>
     <div className='links'>
-        <a className='reference-link1' href='https://github.com/riverferguson/saltys-surf' target='_blank'>Code</a>
+        <a className='reference-link1' href='https://github.com/riverferguson/boat-project' target='_blank'>Code</a>
         <a className='reference-link2' href=''>Demo</a>
     </div>
 </div>

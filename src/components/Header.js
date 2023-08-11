8064fd785187bb9886778dcf66b57f2ddb5e2215
River Ferguson <riverferguson@gmail.com>
2023-08-11 13:17:49 -0700

    style contact underline

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,41 @@ const Header = () => {
   const handleSmoothScroll = (event, target) => {
     event.preventDefault();
     const element = document.querySelector(target);
-    const offset = 80; 
-    const bodyRect = document.body.getBoundingClientRect().top;
-    const elementRect = element.getBoundingClientRect().top;
-    const elementPosition = elementRect - bodyRect;
-    const offsetPosition = elementPosition - offset;
 
-    window.scrollBy({
-      top: offsetPosition,
-      behavior: 'smooth',
-    });
+    if (element) {
+      const offset = 80;
+      const bodyRect = document.body.getBoundingClientRect().top;
+      const elementRect = element.getBoundingClientRect().top;
+      const elementPosition = elementRect - bodyRect;
+      const offsetPosition = elementPosition - offset;
+
+      const start = window.scrollY;
+      const startTime = performance.now();
+      const duration = 1000;
+
+      const animateScroll = (timestamp) => {
+        const currentTime = timestamp - startTime;
+        const progress = Math.min(currentTime / duration, 1);
+
+        const easedProgress = 0.5 - Math.cos(progress * Math.PI) / 2;
+        const newPosition = start + (offsetPosition - start) * easedProgress;
+
+        window.scrollTo(0, newPosition);
+
+        if (currentTime < duration) {
+          requestAnimationFrame(animateScroll);
+        }
+      };
+
+      requestAnimationFrame(animateScroll);
+    }
   };
 
   return (
     <div className="nav">
       <header className="App-header">
         <div className="header-name">
-          <a className='header-link' href="/">River Ferguson</a>
+          <a className='header-link' href='#about'>River Ferguson</a>
         </div>
         <div className="header-list">
           <ul>
